43e82690e90a38883ea7e9324b6103e5aef962b2
Lari Pulkkinen <lari.pulkkinen@gmail.com>
2021-03-02 23:17:16 +0200

    pio serial compatibility - \r\n

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <WiFi.h>
 
-const size_t strLen = 32;
+const size_t strLen = 33;
 const int SSIDPos = 0;
 const int passwordPos = 32;
 char SSID[strLen];
 char password[strLen];
-boolean waitingForInput;
 
-void ask(char *bytes, const char* msg);
+void printCurrent();
+void ask(char *bytes, const char *msg);
 void writeStr(char *data, int pos);
 void readStr(int pos, char *data, size_t len);
 
@@ -17,49 +18,62 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Configuring Wifi credentials & OTA in EEPROM.");
 
-  EEPROM.begin(2*strLen);
+  EEPROM.begin(512);
 
-  char SSID_OLD[strLen];
-  readStr(SSIDPos, SSID_OLD, strLen);
-  Serial.print("Current SSID: "); Serial.println(SSID_OLD);
+  printCurrent();
 
-  char password_old[strLen];
-  readStr(passwordPos, password_old, strLen);
-  Serial.print("Current password: "); Serial.println(password_old);
-
-  Serial.println();
   ask(SSID, "WiFi SSID: ");
   ask(password, "WiFi password: ");
 
   writeStr(SSID, SSIDPos);
   writeStr(password, passwordPos);
 
-  // Serial.print("Your SSID is "); Serial.println(SSID);
-  // Serial.print("Your password is "); Serial.println(password);
-  //Serial.print("Your password is "); Serial.println(password);
+  Serial.print("Configuration complete, connecting WiFi");
 
-  Serial.println("Bootstrap complete.");
+  WiFi.begin(SSID, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print('.');
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: "); Serial.println(WiFi.localIP());
+
 }
 
-void ask(char *bytes, const char* msg) {
-  waitingForInput = true;
-  static byte ndx;
+void printCurrent() {
+  Serial.println("Current configuration:");
+
+  char SSID_OLD[strLen];
+  readStr(SSIDPos, SSID_OLD, strLen);
+  Serial.print("  WiFi SSID: "); Serial.println(SSID_OLD);
+
+  char password_old[strLen];
+  readStr(passwordPos, password_old, strLen);
+  Serial.print("  WiFi password: "); Serial.println(password_old);
+
+  Serial.println();
+}
+
+void ask(char *bytes, const char *msg) {
+  boolean waitingForInput = true;
+  byte ndx = 0;
   char rc;
 
   Serial.print(msg);
+
   while (waitingForInput) {
     while (Serial.available() > 0) {
       rc = Serial.read();
-      if (rc != '\n') {
+      if (rc != '\n' && rc != '\r') {
         bytes[ndx++] = rc;
         if (ndx >= strLen) ndx = strLen - 1;
       } else {
         bytes[ndx] = '\0';
-        ndx = 0;
         waitingForInput = false;
       }
     }
   }
+  Serial.println();
 }
 
 void writeStr(char *data, int pos) {
@@ -72,13 +86,7 @@ void writeStr(char *data, int pos) {
 }
 
 void readStr(int pos, char *data, const size_t len) {
-  //char *flash = (char*) malloc(len+1);
-  
-
-  for (int i=0; i<len; i++) {
-    data[i] = EEPROM.read(pos+i);
-  }
-  //*data = flash;
+  for (int i=0; i<len; i++) data[i] = EEPROM.read(pos+i);
 }
 
 void loop() {}
